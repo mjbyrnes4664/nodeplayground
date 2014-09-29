f8e6caebdb1286542a1ccf032f7f343662d93709
Mike Byrnes <michael.byrnes@bloomingdales.com>
2014-09-29 16:09:34 -0400

    wheel of fortune app

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-var express = require('express')
-var app = express();
+var express = require('express'),
+	hbs = require('express-hbs'),
+	http = require('http'),
+	app = express(),
+	server = http.createServer(app);
+
+hbs.registerHelper('if_eq', function(a, b, opts) {
+    if(a ==- b) 
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
+app.configure(function(){
+	app.use(express.static(__dirname + '/public'));
+
+	// Use `.hbs` for extensions and find partials in `views/partials`.
+	app.engine('hbs', hbs.express3({
+	  partialsDir: __dirname + '/templates-src',
+	  contentHelperName: 'content',
+	}));
+	app.set('view engine', 'hbs');
+	app.set('views', __dirname + '/templates-src');
+
+	app.engine('html', hbs.express3);
+	app.use(express.bodyParser());
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.get('/wheelOfFortune', function(request, response) {
+  response.render( 'WheelOfFortune', { letters: "this is my puzzle".split( "" ) } );
 })
+
+var port = process.env.PORT || 2500;
+server.listen(port, function() {
+  console.log("LISTENING ON PORT: "+port);
+});
---
+++ public/js/wheelOfFortune.js
@@ -0,0 +1,29 @@
+(function(){
+	
+	function guessLetter(){
+		var letter = this.id,
+			$this = $(this),
+			lettersInPuzzle = $( ".letter_" + letter ),
+			result = $("#result");
+			numberOfMatches = lettersInPuzzle.length,
+			resultMessage = "";
+		if($this.hasClass("guessed")){
+			return;
+		}
+		
+		if( numberOfMatches === 0 ){
+			resultMessage = "There are no " + letter.toUpperCase() + "s in the puzzle";
+		} else if( numberOfMatches === 1 ){
+			resultMessage = "There is 1 " + letter.toUpperCase() + " in the puzzle";
+		} else {
+			resultMessage = "These are " + numberOfMatches + " " + letter.toUpperCase() + "s in the puzzle";
+		}
+		
+		result.html(resultMessage);
+		
+		lettersInPuzzle.addClass( "visible" );
+		$this.addClass( "guessed" );
+	}
+	
+	$(document).on("click", ".guessLetter", guessLetter);
+})();
